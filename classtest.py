94f79b401e7d0393a86fa8002f12dcbbc3a04096
Red-C0der <r3dc0d1ng@gmail.com>
2016-04-15 15:58:09 +0200

    Changes unittest stuff.

--- classtest.py
+++ classtest.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Red_C0der'
-
 import unittest
+__author__ = 'Red_C0der'
 
 
 class Decide:
@@ -14,23 +13,33 @@ class Decide:
             inp = raw_input("=> ")
 
 
-
 class Test(unittest.TestCase):
 
     def testDecide(self):
         pass
 
+
 def calc(n):
+    """
+    Calculate n+1
+    """
     return n+1
+
 
 class CheckCalc(unittest.TestCase):
 
-    def test_calc(self):
-        for i in range(5):
+    def test_calc_small(self):
             self.assertEqual(calc(2), 3)
             self.assertEqual(calc(14), 15)
-            self.assertEqual(calc(544), 545)
-            #self.assertEqual(calc(4), 6)
+            self.assertEqual(calc(34), 35)
+            self.assertEqual(calc(87), 88)
+#           self.assertEqual(calc(4), 6)
+
+    def test_calc_big(self):
+        self.assertEqual(calc(100), 101)
+        self.assertEqual(calc(362), 363)
+        self.assertEqual(calc(2715), 2716)
+        self.assertEqual(calc(291616), 291617)
 
 if __name__ == "__main__":
     unittest.main()
