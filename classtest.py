116678577ded5172204427db93500eaad2069f84
Red-C0der <r3dc0d1ng@gmail.com>
2016-04-15 15:11:10 +0200

    Changed classtest.py for testing purposes.

--- classtest.py
+++ classtest.py
@@ -1,17 +1,35 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Red_C0der'
 
-import unittest, mock
+import unittest
+
+
+class Decide:
+
+    def __init__(self):
+        self.history = []
+
+    def loop(self):
+        while True:
+            inp = raw_input("=> ")
+
+
+
+class Test(unittest.TestCase):
+
+    def testDecide(self):
+        pass
 
 def calc(n):
     return n+1
-
 
 class CheckCalc(unittest.TestCase):
 
     def test_calc(self):
         for i in range(5):
             self.assertEqual(calc(2), 3)
+            self.assertEqual(calc(14), 15)
+            self.assertEqual(calc(544), 545)
             #self.assertEqual(calc(4), 6)
 
 if __name__ == "__main__":
